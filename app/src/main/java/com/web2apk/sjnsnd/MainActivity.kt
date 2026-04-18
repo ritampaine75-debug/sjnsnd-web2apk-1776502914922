84f27b15192c03a8fd7b0ee098c8a1565fe0910f
ritampaine75-debug <ritampaine75@gmail.com>
2026-04-18 14:31:58 +0530

    Add app/src/main/java/com/web2apk/sjnsnd/MainActivity.kt

---
+++ app/src/main/java/com/web2apk/sjnsnd/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.web2apk.sjnsnd
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://apk-xi-ten.vercel.app/")
+        setContentView(webView)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
